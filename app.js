dd5447fd5e534735abc2ba8b9262380528ce130f
olya Filipovich <olenenokolya1988@gmail.com>
2022-10-30 18:09:35 +0300

    fix: add classes to tags in html according to BEM; edit name selectors (not all) in css; add and edit some properties in js accordin to rule 3.1-3.4 and 2.1(high level) in html-and-css.md

--- app.js
+++ app.js
@@ -32,18 +32,21 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = "items-todo__list-item";
+
     label.innerText=taskString;
-    label.className="task";
+    label.className="items-todo__label task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "input-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="items-todo__input input-text task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete btn";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
